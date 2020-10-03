1ac503bed35acc3f706372ca1d0104677830ec87
ilam56 <ilam56@mail.missouri.edu>
2020-10-02 19:26:14 -0500

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FormComponent } from './form/form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, FormComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -9,12 +10,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FormComponent implements OnInit {
 
-  SERVER_URL = "https://ilam56.com:8080/exploration1/submit";
-  uploadForm: FormGroup;  
+  form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
-
-  ngOnInit() {
+  constructor(public fb: FormBuilder, private http: HttpClient) {
+    this.form = this.fb.group({
+      name: [''],
+      character: ['']
+    })
   }
+
+  ngOnInit() { }
+
+  uploadFile(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      avatar: file
+    });
+    this.form.get('avatar').updateValueAndValidity()
+  }
+
+  submitForm() {
+    var formData: any = new FormData();
+    formData.append("name", "test");
+    formData.append("character", this.form.get('character').value);
+
+    this.http.post('https://www.ilam56.com:8080/exploration1/submit', formData)
+
+  }
+
 
 }
